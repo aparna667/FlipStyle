ebb821b219d5df04dfd2aef44d59c5da782f2f5c
Abhav <goelabhav2002@gmail.com>
2023-08-17 03:24:59 +0530

    updated the scraper.py to scrape pinterest

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 #main app for the project
+import streamlit as st 
+# import Streamlit library
+
+from pinscrape import pinscrape
+details = pinscrape.scraper.scrape("messi", "output", {}, 10, 15)
+
+if details["isDownloaded"]:
+    print("\nDownloading completed !!")
+    print(f"\nTotal urls found: {len(details['extracted_urls'])}")
+    print(f"\nTotal images downloaded (including duplicate images): {len(details['url_list'])}")
+    print(details)
+else:
+    print("\nNothing to download !!")
--- scraper.py
+++ scraper.py
@@ -1,32 +1,19 @@
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
-def scrape_pinterest_tags(search_query):
-    url = f"https://www.pinterest.com/search/pins/?q={search_query}"
-    response = requests.get(url)
-    print(response)
-    
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html5lib')
-        # print(soup)
-        tag_elements = soup.find_all('div', class_='Yl- MIw Hb7')
-        print(tag_elements)
-        
-        tags = []
-        for tag_element in tag_elements:
-            tag = tag_element.get_text()
-            tags.append(tag)
-        
-        return tags
-    else:
-        print("Failed to retrieve the page.")
-        return None
+# Set up a headless Chrome browser
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
 
-search_query = "fashion"
-tags = scrape_pinterest_tags(search_query)
+url = "https://www.pinterest.com/search/pins/?q=fashion"  # Example URL
+driver.get(url)
 
-if tags:
-    print("Tags:")
-    for tag in tags:
-        print(tag)
+driver.implicitly_wait(10)  
 
+page_source = driver.page_source
+
+driver.quit()
+
+soup = BeautifulSoup(page_source, 'html.parser')
+print(soup.body.div.div.prettify())
